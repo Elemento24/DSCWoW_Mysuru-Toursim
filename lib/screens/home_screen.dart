e041d56e608ba5549659d8fd44df00850d4887d4
Elemento24 <mittalvishesh021@gmail.com>
2020-12-11 16:28:22 +0530

    Add the Home Image & make the Home Container

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -7,8 +7,61 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Home Screen'),
       ),
-      body: Center(
-        child: Text('Home Screen'),
+      body: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.all(20),
+            width: double.infinity,
+            height: 200,
+            child: Stack(
+              children: [
+                Image(
+                  image: AssetImage('assets/images/1.webp'),
+                  fit: BoxFit.cover,
+                  height: 200,
+                  width: double.infinity,
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.bottomCenter,
+                      end: Alignment.topCenter,
+                      colors: [
+                        Theme.of(context).primaryColor,
+                        Colors.transparent,
+                      ],
+                    ),
+                  ),
+                ),
+                Container(
+                  width: double.infinity,
+                  height: 200,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Mysore',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 32,
+                        ),
+                      ),
+                      Text(
+                        'The Cultural Capital of Karnataka',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontStyle: FontStyle.italic,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
