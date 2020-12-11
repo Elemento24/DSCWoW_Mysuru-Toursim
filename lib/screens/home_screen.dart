39788eefd6c1ac5182a538aa3568a8c56cc4d81b
Elemento24 <mittalvishesh021@gmail.com>
2020-12-11 16:56:40 +0530

    Add some Images of Msyore, and complete the About Mysore Section

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -5,63 +7,132 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home Screen'),
+        title: Text('Mysuru: One of it\'s Kind'),
       ),
-      body: Column(
-        children: [
-          Container(
-            margin: EdgeInsets.all(20),
-            width: double.infinity,
-            height: 200,
-            child: Stack(
-              children: [
-                Image(
-                  image: AssetImage('assets/images/1.webp'),
-                  fit: BoxFit.cover,
-                  height: 200,
-                  width: double.infinity,
-                ),
-                Container(
-                  decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                      begin: Alignment.bottomCenter,
-                      end: Alignment.topCenter,
-                      colors: [
-                        Theme.of(context).primaryColor,
-                        Colors.transparent,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              margin: EdgeInsets.all(20),
+              width: double.infinity,
+              height: 200,
+              child: Stack(
+                children: [
+                  Image(
+                    image: AssetImage('assets/images/1.webp'),
+                    fit: BoxFit.cover,
+                    height: 200,
+                    width: double.infinity,
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        begin: Alignment.bottomCenter,
+                        end: Alignment.topCenter,
+                        colors: [
+                          Theme.of(context).primaryColor,
+                          Colors.transparent,
+                        ],
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: double.infinity,
+                    height: 200,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Mysore',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 32,
+                          ),
+                        ),
+                        Text(
+                          'The Cultural Capital of Karnataka',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontStyle: FontStyle.italic,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
                       ],
                     ),
                   ),
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(
+                horizontal: 20,
+              ),
+              padding: EdgeInsets.all(20),
+              width: double.infinity,
+              // height: 300,
+              decoration: BoxDecoration(
+                gradient: RadialGradient(
+                  center: Alignment(0.0, 0.0),
+                  radius: .5,
+                  colors: [
+                    Colors.white,
+                    Theme.of(context).primaryColor,
+                  ],
                 ),
-                Container(
-                  width: double.infinity,
-                  height: 200,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
+              ),
+              child: Column(
+                children: [
+                  Text(
+                    'About Mysore',
+                    style: TextStyle(
+                      fontSize: 24,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    'The cultural ambiance and achievements of Mysuru earned it the sobriquet Cultural Capital of Karnataka. Mysuru is noted for its heritage structures and palaces, including the Mysuru Palace, and for the festivities that take place during the Dasara festival when the city receives many tourists from around the world.',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontStyle: FontStyle.italic,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Text(
-                        'Mysore',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 32,
-                        ),
+                      Image(
+                        image: AssetImage('assets/images/2.jpeg'),
+                        fit: BoxFit.cover,
+                        height: 100,
+                        width: 100,
                       ),
-                      Text(
-                        'The Cultural Capital of Karnataka',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 20,
-                          fontStyle: FontStyle.italic,
-                          fontWeight: FontWeight.bold,
-                        ),
+                      Image(
+                        image: AssetImage('assets/images/3.jpeg'),
+                        fit: BoxFit.cover,
+                        height: 100,
+                        width: 100,
+                      ),
+                      Image(
+                        image: AssetImage('assets/images/4.jpeg'),
+                        fit: BoxFit.cover,
+                        height: 100,
+                        width: 100,
                       ),
                     ],
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          )
-        ],
+            Container(
+              margin: EdgeInsets.all(20),
+            ),
+          ],
+        ),
       ),
     );
   }
