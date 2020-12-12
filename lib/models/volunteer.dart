e6e765876cddd40994985311c4560c4925a6fab7
Elemento24 <mittalvishesh021@gmail.com>
2020-12-12 21:04:33 +0530

    Create a Model for the Volunteer

---
+++ lib/models/volunteer.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:mysuru_toursim/models/review.dart';
+
+class Volunteer {
+  final String id;
+  final String userId;
+  final String title;
+  final String description;
+  final bool isCab;
+  final List<Review> reviews;
+
+  Volunteer({
+    @required this.id,
+    @required this.userId,
+    @required this.title,
+    @required this.description,
+    @required this.isCab,
+    this.reviews,
+  });
+}
