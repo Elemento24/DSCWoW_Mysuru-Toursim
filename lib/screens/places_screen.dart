47db868051cde0ba591ef5e7f3bfdbc597fd7c4e
Elemento24 <mittalvishesh021@gmail.com>
2020-12-12 18:13:00 +0530

    Use the fetchAndSetPlaces

--- lib/screens/places_screen.dart
+++ lib/screens/places_screen.dart
@@ -6,8 +6,32 @@ import '../widgets/app_drawer.dart';
 import '../widgets/place_card.dart';
 import '../providers/places.dart';
 
-class PlacesScreen extends StatelessWidget {
+class PlacesScreen extends StatefulWidget {
   static const routeName = '/places';
+
+  @override
+  _PlacesScreenState createState() => _PlacesScreenState();
+}
+
+class _PlacesScreenState extends State<PlacesScreen> {
+  var _isInit = true;
+  var _isLoading = false;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    if (_isInit) {
+      setState(() {
+        _isLoading = true;
+      });
+      Provider.of<Places>(context).fetchAndSetPlaces().then((_) {
+        setState(() {
+          _isLoading = false;
+        });
+      });
+    }
+    _isInit = false;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,21 +41,23 @@ class PlacesScreen extends StatelessWidget {
         title: Text('Places Screen'),
       ),
       drawer: AppDrawer(),
-      body: SingleChildScrollView(
-        child: Column(
-          children: places
-              .map(
-                (el) => InkWell(
-                  onTap: () {
-                    Navigator.of(context)
-                        .pushNamed(PlaceScreen.routeName, arguments: el.id);
-                  },
-                  child: PlaceCard(el.title, el.imageUrl),
-                ),
-              )
-              .toList(),
-        ),
-      ),
+      body: _isLoading
+          ? Center(child: CircularProgressIndicator())
+          : SingleChildScrollView(
+              child: Column(
+                children: places
+                    .map(
+                      (el) => InkWell(
+                        onTap: () {
+                          Navigator.of(context).pushNamed(PlaceScreen.routeName,
+                              arguments: el.id);
+                        },
+                        child: PlaceCard(el.title, el.imageUrl),
+                      ),
+                    )
+                    .toList(),
+              ),
+            ),
     );
   }
 }
