a5e6a1028e6e864e19283b4b5aa850c5418eb2d2
Elemento24 <mittalvishesh021@gmail.com>
2020-12-12 18:12:42 +0530

    Add the fetchAndSetPlaces functionality

--- lib/providers/places.dart
+++ lib/providers/places.dart
@@ -1,86 +1,88 @@
-// import 'dart:convert';
+import 'dart:convert';
 
 import 'package:flutter/material.dart';
-import 'package:mysuru_toursim/models/review.dart';
-// import 'package:http/http.dart' as http;
+import 'package:http/http.dart' as http;
 
+// import '../models/hotel.dart';
+import '../models/review.dart';
 import '../models/place.dart';
 
 class Places with ChangeNotifier {
   List<Place> _places = [
-    Place(
-      id: DateTime.now().toIso8601String(),
-      title: 'The Temple',
-      address: 'Pranshu Ka Ghar, 4220011',
-      description:
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed hendrerit tincidunt porta. Cras nec luctus diam. Phasellus iaculis diam vel dui rutrum, eget ullamcorper lectus commodo. Sed mollis aliquet lacus ut auctor. Interdum et malesuada fames ac ante ipsum primis in faucibus',
-      imageUrl:
-          'https://images.livemint.com/rf/Image-621x414/LiveMint/Period1/2015/08/10/Photos/mys-kPGB--621x414@LiveMint.jpg',
-      placeReview: [
-        Review(
-          author: 'Avneesh Kumar',
-          message:
-              'A great place to visit. Seems like i was in heaven. Absolutely fantastic place',
-          rating: 4.9,
-        ),
-        Review(
-          author: 'Avneesh Kumar',
-          message:
-              'A great place to visit. Seems like i was in heaven. Absolutely fantastic place',
-          rating: 4.9,
-        ),
-        Review(
-          author: 'Avneesh Kumar',
-          message:
-              'A great place to visit. Seems like i was in heaven. Absolutely fantastic place',
-          rating: 4.9,
-        ),
-        Review(
-          author: 'Avneesh Kumar',
-          message:
-              'A great place to visit. Seems like i was in heaven. Absolutely fantastic place',
-          rating: 4.9,
-        ),
-      ],
-      bestTimeToVisit: 'Evening',
-    ),
-    Place(
-      id: DateTime.now().toIso8601String(),
-      title: 'The Canyon',
-      address: 'Vishesh Ka Ghar,200019',
-      description:
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed hendrerit tincidunt porta. Cras nec luctus diam. Phasellus iaculis diam vel dui rutrum, eget ullamcorper lectus commodo. Sed mollis aliquet lacus ut auctor. Interdum et malesuada fames ac ante ipsum primis in faucibus',
-      imageUrl:
-          'https://images.livemint.com/rf/Image-621x414/LiveMint/Period1/2015/08/10/Photos/mys-kPGB--621x414@LiveMint.jpg',
-      placeReview: [
-        Review(
-          author: 'Avneesh Kumar',
-          message:
-              'A great place to visit. Seems like i was in heaven. Absolutely fantastic place',
-          rating: 4.9,
-        ),
-        Review(
-          author: 'Avneesh Kumar',
-          message:
-              'A great place to visit. Seems like i was in heaven. Absolutely fantastic place',
-          rating: 4.9,
-        ),
-        Review(
-          author: 'Avneesh Kumar',
-          message:
-              'A great place to visit. Seems like i was in heaven. Absolutely fantastic place',
-          rating: 4.9,
-        ),
-        Review(
-          author: 'Avneesh Kumar',
-          message:
-              'A great place to visit. Seems like i was in heaven. Absolutely fantastic place',
-          rating: 4.9,
-        ),
-      ],
-      bestTimeToVisit: 'Afternoon',
-    ),
+    // Place(
+    //   id: DateTime.now().toIso8601String(),
+    //   title: 'The Temple',
+    //   address: 'Pranshu Ka Ghar, 4220011',
+    //   description:
+    //       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed hendrerit tincidunt porta. Cras nec luctus diam. Phasellus iaculis diam vel dui rutrum, eget ullamcorper lectus commodo. Sed mollis aliquet lacus ut auctor. Interdum et malesuada fames ac ante ipsum primis in faucibus',
+    //   imageUrl:
+    //       'https://images.livemint.com/rf/Image-621x414/LiveMint/Period1/2015/08/10/Photos/mys-kPGB--621x414@LiveMint.jpg',
+    //   placeReview: [
+    //     Review(
+    //       author: 'Avneesh Kumar',
+    //       message:
+    //           'A great place to visit. Seems like i was in heaven. Absolutely fantastic place',
+    //       rating: 4.9,
+    //     ),
+    //     Review(
+    //       author: 'Avneesh Kumar',
+    //       message:
+    //           'A great place to visit. Seems like i was in heaven. Absolutely fantastic place',
+    //       rating: 4.9,
+    //     ),
+    //     Review(
+    //       author: 'Avneesh Kumar',
+    //       message:
+    //           'A great place to visit. Seems like i was in heaven. Absolutely fantastic place',
+    //       rating: 4.9,
+    //     ),
+    //     Review(
+    //       author: 'Avneesh Kumar',
+    //       message:
+    //           'A great place to visit. Seems like i was in heaven. Absolutely fantastic place',
+    //       rating: 4.9,
+    //     ),
+    //   ],
+    //   bestTimeToVisit: 'Evening',
+    // ),
+    // Place(
+    //   id: DateTime.now().toIso8601String(),
+    //   title: 'The Canyon',
+    //   address: 'Vishesh Ka Ghar,200019',
+    //   description:
+    //       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed hendrerit tincidunt porta. Cras nec luctus diam. Phasellus iaculis diam vel dui rutrum, eget ullamcorper lectus commodo. Sed mollis aliquet lacus ut auctor. Interdum et malesuada fames ac ante ipsum primis in faucibus',
+    //   imageUrl:
+    //       'https://images.livemint.com/rf/Image-621x414/LiveMint/Period1/2015/08/10/Photos/mys-kPGB--621x414@LiveMint.jpg',
+    //   placeReview: [
+    //     Review(
+    //       author: 'Avneesh Kumar',
+    //       message:
+    //           'A great place to visit. Seems like i was in heaven. Absolutely fantastic place',
+    //       rating: 4.9,
+    //     ),
+    //     Review(
+    //       author: 'Avneesh Kumar',
+    //       message:
+    //           'A great place to visit. Seems like i was in heaven. Absolutely fantastic place',
+    //       rating: 4.9,
+    //     ),
+    //     Review(
+    //       author: 'Avneesh Kumar',
+    //       message:
+    //           'A great place to visit. Seems like i was in heaven. Absolutely fantastic place',
+    //       rating: 4.9,
+    //     ),
+    //     Review(
+    //       author: 'Avneesh Kumar',
+    //       message:
+    //           'A great place to visit. Seems like i was in heaven. Absolutely fantastic place',
+    //       rating: 4.9,
+    //     ),
+    //   ],
+    //   bestTimeToVisit: 'Afternoon',
+    // ),
   ];
+  // final String authToken;
 
   List<Place> get places {
     return [..._places];
@@ -95,5 +97,38 @@ class Places with ChangeNotifier {
     print(desc);
     print(author);
     print(rating);
+  }
+
+  Future<void> fetchAndSetPlaces() async {
+    final url =
+        'https://mysuru-tourism-7d522-default-rtdb.firebaseio.com//places.json';
+    try {
+      final response = await http.get(url);
+
+      // final extractedData = json.decode(response.body) as Map<String, dynamic>;
+      final extractedData = json.decode(response.body);
+
+      if (extractedData == null) return;
+      print(extractedData);
+
+      final List<Place> loadedPlaces = [];
+      extractedData.forEach((placeId, data) {
+        loadedPlaces.add(Place(
+          id: placeId,
+          address: data['address'],
+          description: data['description'],
+          imageUrl: data['imageUrl'],
+          title: data['title'],
+          bestTimeToVisit: data['bestTimeToVisit'],
+          placeReview: [],
+        ));
+      });
+      _places = loadedPlaces;
+      print(_places);
+      notifyListeners();
+    } catch (error) {
+      print(error);
+      throw (error);
+    }
   }
 }
