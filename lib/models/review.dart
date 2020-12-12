4977db07634759a38731cfe6c54a462137573f14
Elemento24 <mittalvishesh021@gmail.com>
2020-12-12 18:56:42 +0530

    Modify the User Model

--- lib/models/review.dart
+++ lib/models/review.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 
 class Review {
+  String id;
+  String creatorId;
   String author;
   String message;
   double rating;
 
-  Review(
-      {@required this.author,
-      @required this.message,
-      @required this.rating});
+  Review({
+    @required this.id,
+    @required this.creatorId,
+    @required this.author,
+    @required this.message,
+    @required this.rating,
+  });
 }
