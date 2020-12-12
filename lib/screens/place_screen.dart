fb0329babdf12a2d5124084d6d14846756eda9b8
Elemento24 <mittalvishesh021@gmail.com>
2020-12-12 16:26:15 +0530

    Refactor the Place Screen and include the Add Review functionality

--- lib/screens/place_screen.dart
+++ lib/screens/place_screen.dart
@@ -1,21 +1,45 @@
 import 'package:flutter/material.dart';
-import 'package:mysuru_toursim/widgets/divider_custom.dart';
-import 'package:mysuru_toursim/widgets/review_card.dart';
 import 'package:provider/provider.dart';
 
+import '../widgets/divider_custom.dart';
+import '../widgets/review_card.dart';
+import '../widgets/new_review.dart';
 import '../providers/places.dart';
 
 class PlaceScreen extends StatelessWidget {
   static const routeName = '/place';
+  Places _placeProvider;
+  String _placeId;
+
+  void _addReview(String desc, double rating, String author) {
+    _placeProvider.addReview(
+      id: _placeId,
+      author: author,
+      desc: desc,
+      rating: rating,
+    );
+  }
+
+  void _startAddNewRev(BuildContext ctx) {
+    showModalBottomSheet(
+      context: ctx,
+      builder: (_) {
+        return NewReview(_addReview);
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    final id = ModalRoute.of(context).settings.arguments as String;
-    final place = Provider.of<Places>(context, listen: false).findById(id);
+    _placeId = ModalRoute.of(context).settings.arguments as String;
+    final place =
+        Provider.of<Places>(context, listen: false).findById(_placeId);
+    _placeProvider = Provider.of<Places>(context, listen: false);
 
     return Scaffold(
       appBar: AppBar(
         title: Text(place.title),
+        elevation: 0,
       ),
       body: Container(
         height: double.infinity,
@@ -33,9 +57,27 @@ class PlaceScreen extends StatelessWidget {
                         Container(
                           height: MediaQuery.of(context).size.height * 0.3,
                           width: double.infinity,
-                          child: Image.network(
-                            place.imageUrl,
-                            fit: BoxFit.cover,
+                          child: Stack(
+                            children: [
+                              Image(
+                                image: NetworkImage(place.imageUrl),
+                                fit: BoxFit.cover,
+                                height: 250,
+                                width: double.infinity,
+                              ),
+                              Container(
+                                decoration: BoxDecoration(
+                                  gradient: LinearGradient(
+                                    begin: Alignment.topCenter,
+                                    end: Alignment.bottomCenter,
+                                    colors: [
+                                      Theme.of(context).primaryColor,
+                                      Colors.transparent,
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ],
                           ),
                         ),
                         SizedBox(
@@ -45,6 +87,7 @@ class PlaceScreen extends StatelessWidget {
                         Padding(
                           padding: EdgeInsets.all(8),
                           child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
                             children: <Widget>[
                               Icon(
                                 Icons.location_pin,
@@ -53,7 +96,9 @@ class PlaceScreen extends StatelessWidget {
                               Text(
                                 "${place.address}",
                                 style: TextStyle(
-                                    fontStyle: FontStyle.italic, fontSize: 14),
+                                  fontStyle: FontStyle.italic,
+                                  fontSize: 14,
+                                ),
                               ),
                             ],
                           ),
@@ -61,22 +106,32 @@ class PlaceScreen extends StatelessWidget {
                         Padding(
                           padding: EdgeInsets.all(8),
                           child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
                             children: [
                               Icon(Icons.watch_sharp),
                               Text(
                                 "${place.bestTimeToVisit}",
                                 style: TextStyle(
-                                    fontSize: 16, fontWeight: FontWeight.bold, fontStyle: FontStyle.italic),
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                  fontStyle: FontStyle.italic,
+                                ),
                               )
                             ],
                           ),
                         ),
                         DividerCustom(),
                         Padding(
-                          padding: EdgeInsets.all(8),
+                          padding: EdgeInsets.symmetric(
+                            vertical: 14,
+                            horizontal: 20,
+                          ),
                           child: Text(
                             place.description,
-                            style: TextStyle(fontSize: 18),
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontStyle: FontStyle.italic,
+                            ),
                           ),
                         ),
                       ],
@@ -94,7 +149,9 @@ class PlaceScreen extends StatelessWidget {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-          onPressed: () {}, child: Icon(Icons.rate_review)),
+        onPressed: () => _startAddNewRev(context),
+        child: Icon(Icons.rate_review),
+      ),
     );
   }
 }
